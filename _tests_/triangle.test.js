706c1bcd95c930afdc3c281cb79fd924c44e4234
Brit Wallace <britwallace888@gmail.com>
2021-12-12 15:40:26 -0800

    Test passed for determining if not a trangle or a scalene triangle

--- _tests_/triangle.test.js
+++ _tests_/triangle.test.js
@@ -9,3 +9,7 @@ describe('Triangle', () => {
     expect(triangle.side3).toEqual(5);
   });
 });
+test('should correctly determine whether three lengths make a scalene triangle', () => {
+  const scalTriangle = new Triangle(4,5,7)
+  expect(scalTriangle.checkType()).toEqual('scalene triangle');
+});
