c66bf31ef980f3a28c48eb452a188f3ccb96cc26
Brit Wallace <britwallace888@gmail.com>
2021-12-22 19:46:57 -0800

    Quad code passes
    Co-authored-by: Karl Starkweather <stark13@usa.net>
    Co-authored-by: Chris Nakayama <cnakayam@gmail.com

--- _tests_/quadrilateral.test.js
+++ _tests_/quadrilateral.test.js
@@ -2,9 +2,10 @@ import Quadrilateral from '../src/quadrilateral.js';
 
 describe('Quadrilateral', () => {
   test('should correctly create a quadrilateral object with four lengths', () => {
-    const quadrilateral = new quadrilateral(2,3,4,5,);
+    const quadrilateral = new Quadrilateral(2,3,4,5);
     expect(quadrilateral.side1).toEqual(2);
     expect(quadrilateral.side2).toEqual(3);
     expect(quadrilateral.side3).toEqual(4);
+    expect(quadrilateral.side4).toEqual(5);
   });
 });
--- src/quadrilateral.js
+++ src/quadrilateral.js
@@ -1,6 +1,6 @@
 export default function Quadrilateral(side1, side2, side3, side4) {
-  // this.side1 = side1;
-  // this.side2 = side2;
-  // this.side3 = side3;
-  // this.side4 = side4;
+  this.side1 = side1;
+  this.side2 = side2;
+  this.side3 = side3;
+  this.side4 = side4;
 }
